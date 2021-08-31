8633103731052d3fc87a1e9957a6a33dd4bd1e0a
Thon Becker <thon.becker@gmail.com>
2021-08-31 16:44:07 -0500

    chore(deps):upgrade CDK packages

--- src/main/java/com/thonbecker/BibleVerseStack.java
+++ src/main/java/com/thonbecker/BibleVerseStack.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+
 import software.amazon.awscdk.core.BundlingOptions;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.DockerVolume;
@@ -43,100 +44,97 @@ import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.FunctionProps;
 import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.logs.RetentionDays;
+
 import static java.util.Collections.singletonList;
 import static software.amazon.awscdk.core.BundlingOutput.ARCHIVED;
 
 public class BibleVerseStack extends Stack {
 
-        public BibleVerseStack(final Construct scope, final String id) {
-                this(scope, id, null);
-        }
+    public BibleVerseStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
 
-        public BibleVerseStack(final Construct scope, final String id, final StackProps props) {
-                super(scope, id, props);
+        Bucket bucket = new Bucket(this, "Bucket", new BucketProps.Builder()
+                .removalPolicy(RemovalPolicy.DESTROY).autoDeleteObjects(true).build());
 
-                Bucket bucket = new Bucket(this, "Bucket", new BucketProps.Builder()
-                                .removalPolicy(RemovalPolicy.DESTROY).autoDeleteObjects(true).build());
+        List<ISource> sources = new ArrayList<>(1);
+        sources.add(Source.asset("./data"));
 
-                List<ISource> sources = new ArrayList<>(1);
-                sources.add(Source.asset("./data"));
+        BucketDeployment bucketDeployment = new BucketDeployment(this, "BucketDeployment",
+                new BucketDeploymentProps.Builder().sources(sources).destinationBucket(bucket).build());
 
-                BucketDeployment bucketDeployment = new BucketDeployment(this, "BucketDeployment",
-                                new BucketDeploymentProps.Builder().sources(sources).destinationBucket(bucket).build());
+        List<String> BibleVersePackagingInstructions = Arrays.asList("/bin/sh", "-c",
+                "mvn clean install " + "&& cp /asset-input/target/bibleverse.jar /asset-output/");
 
-                List<String> BibleVersePackagingInstructions = Arrays.asList("/bin/sh", "-c",
-                                "mvn clean install " + "&& cp /asset-input/target/bibleverse.jar /asset-output/");
+        BundlingOptions.Builder builderOptions = BundlingOptions.builder()
+                .command(BibleVersePackagingInstructions).image(Runtime.JAVA_11.getBundlingImage())
+                .volumes(singletonList(DockerVolume.builder()
+                        .hostPath(System.getProperty("user.home") + "/.m2/")
+                        .containerPath("/root/.m2/").build()))
+                .user("root").outputType(ARCHIVED);
 
-                BundlingOptions.Builder builderOptions = BundlingOptions.builder()
-                                .command(BibleVersePackagingInstructions).image(Runtime.JAVA_11.getBundlingImage())
-                                .volumes(singletonList(DockerVolume.builder()
-                                                .hostPath(System.getProperty("user.home") + "/.m2/")
-                                                .containerPath("/root/.m2/").build()))
-                                .user("root").outputType(ARCHIVED);
+        Map<String, String> environmentMap = new HashMap<>();
+        environmentMap.put("DATA_BUCKET_NAME", bucket.getBucketName());
 
-                Map<String, String> environmentMap = new HashMap<>();
-                environmentMap.put("DATA_BUCKET_NAME", bucket.getBucketName());
+        Function functionRandomBibleVerse = new Function(this, "FunctionRandomBibleVerse", FunctionProps
+                .builder().runtime(Runtime.JAVA_11)
+                .code(Code.fromAsset("./lambda/",
+                        AssetOptions.builder().bundling(builderOptions.build()).build()))
+                .handler("com.thonbecker.randombibleverse.LambdaHandler").memorySize(1024)
+                .timeout(Duration.seconds(10)).logRetention(RetentionDays.ONE_WEEK)
+                .environment(environmentMap).build());
 
-                Function functionRandomBibleVerse = new Function(this, "FunctionRandomBibleVerse", FunctionProps
-                                .builder().runtime(Runtime.JAVA_11)
-                                .code(Code.fromAsset("./lambda/",
-                                                AssetOptions.builder().bundling(builderOptions.build()).build()))
-                                .handler("com.thonbecker.randombibleverse.LambdaHandler").memorySize(1024)
-                                .timeout(Duration.seconds(10)).logRetention(RetentionDays.ONE_WEEK)
-                                .environment(environmentMap).build());
+        Function functionAboutBibleVerse = new Function(this, "FunctionAboutBibleVerse", FunctionProps.builder()
+                .runtime(Runtime.JAVA_11)
+                .code(Code.fromAsset("./lambda/",
+                        AssetOptions.builder().bundling(builderOptions.build()).build()))
+                .handler("com.thonbecker.about.LambdaHandler").memorySize(1024)
+                .timeout(Duration.seconds(10)).logRetention(RetentionDays.ONE_WEEK)
+                .environment(environmentMap).build());
 
-                Function functionAboutBibleVerse = new Function(this, "FunctionAboutBibleVerse", FunctionProps.builder()
-                                .runtime(Runtime.JAVA_11)
-                                .code(Code.fromAsset("./lambda/",
-                                                AssetOptions.builder().bundling(builderOptions.build()).build()))
-                                .handler("com.thonbecker.about.LambdaHandler").memorySize(1024)
-                                .timeout(Duration.seconds(10)).logRetention(RetentionDays.ONE_WEEK)
-                                .environment(environmentMap).build());
+        bucket.grantRead(functionRandomBibleVerse);
 
-                bucket.grantRead(functionRandomBibleVerse);
+        DomainName domainName = new DomainName(this, "DomainName", DomainNameProps.builder()
+                .domainName("bibleverse.thonbecker.com")
+                .certificate(Certificate.fromCertificateArn(this, "CertificateLookup",
+                        "arn:aws:acm:us-west-2:664759038511:certificate/6bb68629-cffb-40a2-8ba4-dcc2d57259c4"))
+                .build());
 
-                DomainName domainName = new DomainName(this, "DomainName", DomainNameProps.builder()
-                                .domainName("bibleverse.thonbecker.com")
-                                .certificate(Certificate.fromCertificateArn(this, "CertificateLookup",
-                                                "arn:aws:acm:us-west-2:664759038511:certificate/6bb68629-cffb-40a2-8ba4-dcc2d57259c4"))
-                                .build());
+        List<String> corsOrigins = new ArrayList<>(1);
+        corsOrigins.add("*");
 
-                List<String> corsOrigins = new ArrayList<>(1);
-                corsOrigins.add("*");
+        List<CorsHttpMethod> corsHttpMethods = new ArrayList<>(2);
+        corsHttpMethods.add(CorsHttpMethod.GET);
+        corsHttpMethods.add(CorsHttpMethod.OPTIONS);
 
-                List<CorsHttpMethod> corsHttpMethods = new ArrayList<>(2);
-                corsHttpMethods.add(CorsHttpMethod.GET);
-                corsHttpMethods.add(CorsHttpMethod.OPTIONS);
+        List<HttpMethod> httpMethods = new ArrayList<>();
+        httpMethods.add(HttpMethod.GET);
 
-                List<HttpMethod> httpMethods = new ArrayList<>();
-                httpMethods.add(HttpMethod.GET);
+        HttpApi httpApi = new HttpApi(this, "bibleverse-api", HttpApiProps.builder().apiName("bibleverse-api")
+                .createDefaultStage(true)
+                .corsPreflight(CorsPreflightOptions.builder().allowMethods(corsHttpMethods)
+                        .allowOrigins(corsOrigins).build())
+                .defaultDomainMapping(DomainMappingOptions.builder().domainName(domainName).build())
+                .build());
 
-                HttpApi httpApi = new HttpApi(this, "bibleverse-api", HttpApiProps.builder().apiName("bibleverse-api")
-                                .createDefaultStage(true)
-                                .corsPreflight(CorsPreflightOptions.builder().allowMethods(corsHttpMethods)
-                                                .allowOrigins(corsOrigins).build())
-                                .defaultDomainMapping(DomainMappingOptions.builder().domainName(domainName).build())
-                                .build());
+        httpApi.addRoutes(AddRoutesOptions.builder().path("/about").methods(httpMethods)
+                .integration(new LambdaProxyIntegration(LambdaProxyIntegrationProps.builder()
+                        .handler(functionAboutBibleVerse)
+                        .payloadFormatVersion(PayloadFormatVersion.VERSION_2_0).build()))
+                .build());
 
-                httpApi.addRoutes(AddRoutesOptions.builder().path("/about").methods(httpMethods)
-                                .integration(new LambdaProxyIntegration(LambdaProxyIntegrationProps.builder()
-                                                .handler(functionAboutBibleVerse)
-                                                .payloadFormatVersion(PayloadFormatVersion.VERSION_2_0).build()))
-                                .build());
+        httpApi.addRoutes(AddRoutesOptions.builder().path("/random").methods(httpMethods)
+                .integration(new LambdaProxyIntegration(LambdaProxyIntegrationProps.builder()
+                        .handler(functionRandomBibleVerse)
+                        .payloadFormatVersion(PayloadFormatVersion.VERSION_2_0).build()))
+                .build());
 
-                httpApi.addRoutes(AddRoutesOptions.builder().path("/random").methods(httpMethods)
-                                .integration(new LambdaProxyIntegration(LambdaProxyIntegrationProps.builder()
-                                                .handler(functionRandomBibleVerse)
-                                                .payloadFormatVersion(PayloadFormatVersion.VERSION_2_0).build()))
-                                .build());
-
-                ARecord aliasRecord = new ARecord(this, "AliasRecord", ARecordProps.builder()
-                                .recordName("bibleverse.thonbecker.com")
-                                .zone(HostedZone.fromLookup(this, "HostedZoneLookup",
-                                                HostedZoneProviderProps.builder().domainName("thonbecker.com").build()))
-                                .target(RecordTarget.fromAlias(
-                                                new ApiGatewayv2DomainProperties(domainName.getRegionalDomainName(),
-                                                                domainName.getRegionalHostedZoneId())))
-                                .build());
-        }
+        ARecord aliasRecord = new ARecord(this, "AliasRecord", ARecordProps.builder()
+                .recordName("bibleverse.thonbecker.com")
+                .zone(HostedZone.fromLookup(this, "HostedZoneLookup",
+                        HostedZoneProviderProps.builder().domainName("thonbecker.com").build()))
+                .target(RecordTarget.fromAlias(
+                        new ApiGatewayv2DomainProperties(domainName.getRegionalDomainName(),
+                                domainName.getRegionalHostedZoneId())))
+                .build());
+    }
 }
