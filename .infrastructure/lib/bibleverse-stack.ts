bce1482fbb7f1e40d00bc1b84f6fb96226bd5081
Thon Becker <thon.becker@ramseysolutions.com>
2022-05-14 18:03:29 -0500

    feat(cdk): add custom lambda

--- .infrastructure/lib/bibleverse-stack.ts
+++ .infrastructure/lib/bibleverse-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 import * as path from "path";
 
 export class BibleVerseStack extends Stack {
@@ -17,6 +18,16 @@ export class BibleVerseStack extends Stack {
     const deployment = new s3deploy.BucketDeployment(this, 'DeployBibleVerse', {
       sources: [s3deploy.Source.asset(path.join(__dirname, 'data'))],
       destinationBucket: bucket,
+    });
+
+    const codeVersion = "stringOrMethodToGetCodeVersion";
+    const fn = new lambda.Function(this, 'MyFunction', {
+      runtime: lambda.Runtime.PROVIDED,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler')),
+      environment: {
+        'DATA_BUCKET_NAME': bucket.bucketName,
+      },
     });
   }
 }
