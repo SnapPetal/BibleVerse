c01f2736dc54f3f4c8533a1bc2ed0127fce94cfb
Thon Becker <thon.becker@ramseysolutions.com>
2022-05-14 08:30:25 -0500

    feat(cdk): add deployment bucket

--- .infrastructure/lib/bibleverse-stack.ts
+++ .infrastructure/lib/bibleverse-stack.ts
@@ -1,6 +1,8 @@
 import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment'
+import * as path from "path";
 
 export class BibleVerseStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -10,6 +12,11 @@ export class BibleVerseStack extends Stack {
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
       removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    const deployment = new s3deploy.BucketDeployment(this, 'DeployBibleVerse', {
+      sources: [s3deploy.Source.asset(path.join(__dirname, 'my-website'))],
+      destinationBucket: bucket,
     });
   }
 }
