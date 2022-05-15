43eaf283fda07dca3db15087304f1ef5c586032e
Thon Becker <thon.becker@ramseysolutions.com>
2022-05-15 13:07:12 -0500

    feat(lambda):adjust lambda configuration

--- lambda/src/main/java/com/thonbecker/bibleverse/BibleVerseApplication.java
+++ lambda/src/main/java/com/thonbecker/bibleverse/BibleVerseApplication.java
@@ -1,14 +1,11 @@
 package com.thonbecker.bibleverse;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BibleVerseApplication {
-
-    @Bean
-    public Function<Void, String> about() {
-        return value -> new JSONStringer().object().key("status").value("healthy").key("version").value("1.0.0").endObject().toString();
+    public static void main(String[] args) {
+        SpringApplication.run(BibleVerseApplication.class, args);
     }
 }
