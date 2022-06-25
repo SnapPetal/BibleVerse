422e4f663efaf8eda6d611121c1cb4f025d7f110
EC2 Default User <ec2-user@ip-172-31-29-46.ec2.internal>
2022-06-25 20:13:27 +0000

    chore:rename CDK stack

--- .infrastructure/bin/bibleverse.ts
+++ .infrastructure/bin/bibleverse.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import {BibleVerseStack} from "../lib/bibleverse-stack";
 
 const app = new cdk.App();
-new BibleVerseStack(app, 'BibleVerseStack', {
+new BibleVerseStack(app, 'bible-verse-stack', {
     env: {
         account: process.env.CDK_DEFAULT_ACCOUNT,
         region: process.env.CDK_DEFAULT_REGION
