d359e06a644d62be1e357fce543e98bcf07fe3c4
Thon Becker <thon.becker@daveramsey.com>
2022-12-16 23:36:59 +0000

    chore:fix logger

--- src/main/java/com/thonbecker/bibleverse/BibleVerseApplication.java
+++ src/main/java/com/thonbecker/bibleverse/BibleVerseApplication.java
@@ -1,19 +1,18 @@
 package com.thonbecker.bibleverse;
 
 import java.util.Arrays;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.ObjectUtils;
 
+@Slf4j
 @SpringBootApplication
-@slf4j
 public class BibleVerseApplication {
   public static void main(String[] args) {
-    logger.info("==> Starting: BibleVerseApplication");
+    log.info("==> Starting: BibleVerseApplication");
     if (!ObjectUtils.isEmpty(args)) {
-      logger.info("==>  args: " + Arrays.asList(args));
+      log.info("==>  args: " + Arrays.asList(args));
     }
     SpringApplication.run(BibleVerseApplication.class, args);
   }
