f6abaa59334da0852787ce3f532c4c0637656ebb
Thon Becker <thon.becker@gmail.com>
2022-12-18 01:31:39 +0000

    chore(deps):upgrade packages

--- src/main/java/com/thonbecker/bibleverse/functions/RandomBibleVerseHandler.java
+++ src/main/java/com/thonbecker/bibleverse/functions/RandomBibleVerseHandler.java
@@ -1,4 +1,4 @@
-package com.thonbecker.bibleverse;
+package com.thonbecker.bibleverse.functions;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
