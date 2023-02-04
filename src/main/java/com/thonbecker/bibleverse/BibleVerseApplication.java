22104a7c78e7109f9185dcbbba514c4371c6bce7
Thon Becker <thon.becker@gmail.com>
2023-02-04 13:12:29 +0000

    chore(deps):upgrade packages and fix build

--- src/main/java/com/thonbecker/bibleverse/BibleVerseApplication.java
+++ src/main/java/com/thonbecker/bibleverse/BibleVerseApplication.java
@@ -1,8 +1,19 @@
 package com.thonbecker.bibleverse;
 
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ObjectUtils;
 
+@Slf4j
 @SpringBootApplication
 public class BibleVerseApplication {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    log.info("==> Starting: BibleVerseApplication");
+    if (!ObjectUtils.isEmpty(args)) {
+      log.info("==>  args: " + Arrays.asList(args));
+    }
+    SpringApplication.run(BibleVerseApplication.class, args);
+  }
 }
