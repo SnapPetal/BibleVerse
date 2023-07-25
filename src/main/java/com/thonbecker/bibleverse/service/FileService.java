fbe1757d4ea1f605e15c2ba9db405b21d400fab4
Thon Becker <me@thonbecker.com>
2023-07-25 00:44:15 +0000

    feat(app): add java container

--- src/main/java/com/thonbecker/bibleverse/service/FileService.java
+++ src/main/java/com/thonbecker/bibleverse/service/FileService.java
@@ -1,18 +1,21 @@
 package com.thonbecker.bibleverse.service;
 
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Service
 public class FileService {
-  private final AmazonS3 s3client =
-      AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
+  private final S3Client s3Client =
+      S3Client.builder()
+          .region(Region.US_EAST_1)
+          .credentialsProvider(DefaultCredentialsProvider.create())
+          .build();
 
   public InputStream getFile(String fileName) {
     return s3client.getObject("bible-verse-data-files", fileName).getObjectContent();
